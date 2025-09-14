ca2b997171a925a80ce8810cf4941284e209fc0c
Martin Quiroga <112977790+MartinQ22@users.noreply.github.com>
2025-09-14 03:41:18 -0300

    ver.0.1

---
+++ index.js
@@ -0,0 +1,46 @@
+// Strings
+let name = "gza"
+
+// Number 
+let id = 1;
+
+//Boolean
+let isAdmin = false;
+
+//Null
+let user = null
+
+//date
+let date = new Date().toLocaleString()
+// (ver la fecha local)
+
+function authUser(){
+
+}
+
+class User{
+
+}
+
+const userN = {
+    name: "Gara",
+    lastname: "Soto",
+
+}
+
+const alumn = ["Matias", "Alessandro" ]
+
+console.log(alumn);
+
+// terminal node ./"nombre del archivo" 
+
+function sumar (num1, num2){
+    const suma = num1 + num2;
+    return suma
+}
+
+const sumarArrow = (num1, num2)=> num1 + num2;
+
+
+console.log(sumarArrow);
+
